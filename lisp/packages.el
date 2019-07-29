a6a8db8dc72079f57947caf2e988e9c382201cde
Ahmed Abdulrahman <ahmed751995@gmail.com>
2019-07-29 15:45:08 +0200

    Update packages.el

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,5 +1,6 @@
-(package install 'color-theme-sanityinc-tomorrow)
+(package-install 'color-theme-sanityinc-tomorrow)
 (package-install 'which-key)
 (package-install 'company)
 (package-install 'autopair)
 (package-install 'company-quickhelp)
+(package-install 'markdown-mode)
