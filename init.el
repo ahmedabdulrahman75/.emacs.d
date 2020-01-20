94670a85a1acc5bceaecb7958be20560cb478204
Ahmed Abdulrahman <ahmed@debian>
2020-01-21 01:26:10 +0200

    added new theme

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
 ;;--------------------------------------------------------------------------
 ;; loading files
 ;;--------------------------------------------------------------------------
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load "~/.emacs.d/general-config") ; general configuration lisp file
 (add-to-list 'load-path "~/.emacs.d/lisp") ; packages configuration file
 (load "packages")
@@ -22,10 +23,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (adwaita)))
+ '(custom-enabled-themes (quote (high-contrast)))
+ '(custom-safe-themes
+   (quote
+    ("8e7044bfad5a2e70dfc4671337a4f772ee1b41c5677b8318f17f046faa42b16b" default)))
  '(package-selected-packages
    (quote
-    (org drag-stuff idle-highlight-mode company-web web-mode helm react-snippets js2-mode company yasnippet yasnippet-snippets which-key treemacs tabbar switch-window rjsx-mode php-mode markdown-mode emmet-mode company-quickhelp autopair))))
+    (org js2-mode company yasnippet yasnippet-snippets which-key web-mode treemacs tabbar switch-window rjsx-mode react-snippets php-mode markdown-mode idle-highlight-mode helm emmet-mode drag-stuff company-web company-quickhelp autopair))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
