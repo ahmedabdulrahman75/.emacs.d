1f62b2fa15e6c4f71742b9072f14dc5b49681d79
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-01-22 18:07:26 +0200

    removed theme

--- init.el
+++ init.el
@@ -10,7 +10,6 @@
 ;;--------------------------------------------------------------------------
 ;; loading files
 ;;--------------------------------------------------------------------------
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load "~/.emacs.d/general-config") ; general configuration lisp file
 (add-to-list 'load-path "~/.emacs.d/lisp") ; packages configuration file
 (load "packages")
@@ -18,21 +17,3 @@
 (load "javascript-lisp")
 (load "web-lisp")
 ;;--------------------------------------------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (high-contrast)))
- '(custom-safe-themes
-   (quote
-    ("8e7044bfad5a2e70dfc4671337a4f772ee1b41c5677b8318f17f046faa42b16b" default)))
- '(package-selected-packages
-   (quote
-    (company-tern org js2-mode company yasnippet yasnippet-snippets which-key web-mode treemacs tabbar switch-window rjsx-mode react-snippets php-mode markdown-mode idle-highlight-mode helm emmet-mode drag-stuff company-web company-quickhelp autopair))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
