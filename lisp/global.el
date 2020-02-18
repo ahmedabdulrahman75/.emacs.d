3e7265c5e2a9e9c7fa362a3481c22d2449aca8a9
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-02-19 01:21:34 +0200

    enable yasnippet with company mode

--- lisp/global.el
+++ lisp/global.el
@@ -47,3 +47,13 @@
 
 ;; hungry delete
 (global-hungry-delete-mode)
+
+;; enable yasnippet with company mode
+(defun autocomplete-show-snippets ()
+  "Show snippets in autocomplete popup."
+  (let ((backend (car company-backends)))
+    (unless (listp backend)
+      (setcar company-backends `(,backend :with company-yasnippet company-files)))))
+;; See http://www.gnu.org/software/emacs/manual/html_node/emacs/Hooks.html
+;; for what this line means
+(add-hook 'after-change-major-mode-hook 'autocomplete-show-snippets)
