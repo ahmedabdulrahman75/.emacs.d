cb8b048a2b67e61377b0cc62e4743bc1d1650729
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-04-11 01:59:42 +0200

    removed powerline

--- lisp/global.el
+++ lisp/global.el
@@ -68,8 +68,3 @@
 (add-hook 'prog-mode-hook #'hs-minor-mode)
 (global-set-key (kbd "C-c <right>") 'hs-show-block)
 (global-set-key (kbd "C-c <left>") 'hs-hide-block)
-
-;; power line
-(add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
-(require 'powerline)
-(require 'cl)
