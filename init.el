4995358536d63e38f4b7ccb94ee450423ea9493b
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-04-14 18:19:31 +0200

    high contrast theme

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
 ;;--------------------------------------------------------------------------
 ;; loading files
 ;;--------------------------------------------------------------------------
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes") ; theme folder
 (load "~/.emacs.d/general-config") ; general configuration lisp file
 (add-to-list 'load-path "~/.emacs.d/lisp") ; packages configuration file
 (load "packages")
@@ -37,14 +38,19 @@
      (output-dvi "xdvi")
      (output-html "xdg-open")
      (output-pdf "Atril"))))
+ '(custom-enabled-themes (quote (high-contrast)))
+ '(custom-safe-themes
+   (quote
+    ("8e7044bfad5a2e70dfc4671337a4f772ee1b41c5677b8318f17f046faa42b16b" default)))
  '(doc-view-continuous t)
  '(package-selected-packages
    (quote
     (auctex company-math org helm tabbar js2-mode company yasnippet yasnippet-snippets which-key web-mode treemacs tabbar-ruler switch-window rjsx-mode react-snippets php-mode markdown-mode json-mode idle-highlight-mode hungry-delete flyspell-correct-helm emmet-mode drag-stuff company-web company-tern company-quickhelp autopair)))
- '(scss-compile-at-save nil))
+ '(scss-compile-at-save nil)
+ '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal)))))
