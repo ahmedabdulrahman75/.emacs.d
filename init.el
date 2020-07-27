f8faa3211cdf2d2735080605c104ced32189c6ea
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-07-27 06:45:38 +0200

    fix reconfigure

--- init.el
+++ init.el
@@ -18,18 +18,3 @@
 ;; (load "web-lisp")
 ;; (load "latex-lisp")
 ;;--------------------------------------------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doc-view-continuous t)
- '(package-selected-packages
-   (quote
-    (dap-mode helm-lsp lsp-treemacs flycheck lsp-ui lsp-mode markdown-mode+ org helm tabbar js2-mode company yasnippet yasnippet-snippets which-key web-mode treemacs tabbar-ruler switch-window scss-mode rjsx-mode react-snippets php-mode markdown-mode json-mode idle-highlight-mode hungry-delete flyspell-correct-helm emmet-mode drag-stuff company-web company-tern company-quickhelp company-math autopair auctex))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
