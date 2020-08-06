711aa318c70f78bf8b26be118abab4849c2b588e
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-08-06 17:36:22 +0200

    reconfig

--- general-config.el
+++ general-config.el
@@ -4,42 +4,42 @@
 ;; hide welcome page
 (setq inhibit-startup-screen t)
 
-;; smart inference of indentation style
-(defun infer-indentation-style ()
-  ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
-  ;; neither, we use the current indent-tabs-mode                               
-  (let ((space-count (how-many "^  " (point-min) (point-max)))
-        (tab-count (how-many "^\t" (point-min) (point-max))))
-    (if (> space-count tab-count) (setq indent-tabs-mode nil))
-    (if (> tab-count space-count) (setq indent-tabs-mode t))))
+;; ;; smart inference of indentation style
+;; (defun infer-indentation-style ()
+;;   ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
+;;   ;; neither, we use the current indent-tabs-mode                               
+;;   (let ((space-count (how-many "^  " (point-min) (point-max)))
+;;         (tab-count (how-many "^\t" (point-min) (point-max))))
+;;     (if (> space-count tab-count) (setq indent-tabs-mode nil))
+;;     (if (> tab-count space-count) (setq indent-tabs-mode t))))
 
-;; don't accelerate scrolling
-(setq mouse-wheel-progressive-speed nil)
+;; ;; don't accelerate scrolling
+;; (setq mouse-wheel-progressive-speed nil)
 
 
-;; Delete selection mode
-(delete-selection-mode 1)
+;; ;; Delete selection mode
+;; (delete-selection-mode 1)
 
-;; enable spell check by default
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+;; ;; enable spell check by default
+;; (add-hook 'text-mode-hook 'flyspell-mode)
+;; (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 ;; auto refresh all buffers
-(global-auto-revert-mode t)
+(global-auto-revert-mode 1)
 
-;; line number in all buffers
-(global-linum-mode t)
-(add-hook 'doc-view-mode-hook (lambda() (linum-mode 0)))
+;; ;; line number in all buffers
+;; (global-linum-mode t)
+;; (add-hook 'doc-view-mode-hook (lambda() (linum-mode 0)))
 
-;; show parenthesis matched
-(show-paren-mode t)
+;; ;; show parenthesis matched
+;; (show-paren-mode t)
 
-;; change the highlight color
-(set-face-attribute 'region nil :background "#B9B9B9")
+;; ;; change the highlight color
+;; (set-face-attribute 'region nil :background "#B9B9B9")
 
 
-;; right to left config
-(defun set-bidi-env ()
-  "interactive"
-  (setq bidi-paragraph-direction 'nil))
-(add-hook 'org-mode-hook 'set-bidi-env)
+;; ;; right to left config
+;; (defun set-bidi-env ()
+;;   "interactive"
+;;   (setq bidi-paragraph-direction 'nil))
+;; (add-hook 'org-mode-hook 'set-bidi-env)
--- init.el
+++ init.el
@@ -10,7 +10,7 @@
 ;;--------------------------------------------------------------------------
 ;; loading files
 ;;--------------------------------------------------------------------------
-;; (load "~/.emacs.d/general-config") ; general configuration lisp file
+(load "~/.emacs.d/general-config") ; general configuration lisp file
 ;; (add-to-list 'load-path "~/.emacs.d/lisp") ; packages configuration file
 ;; (load "packages")
 ;; (load "global")
@@ -18,3 +18,15 @@
 ;; (load "web-lisp")
 ;; (load "latex-lisp")
 ;;--------------------------------------------------------------------------
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (adwaita))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
