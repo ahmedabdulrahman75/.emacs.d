14644f3231f6d9bff8bec7bad9dc78f8709e71c9
Ahmed Abdulrahman <ahmed751995@riseup.net>
2020-09-01 13:51:31 +0200

    reconfig

--- general-config.el
+++ general-config.el
@@ -7,6 +7,9 @@
 ;; show column
 (setq column-number-mode t)
 
+;; stop beep sound
+(setq visible-bell t)
+
 ;; ;; smart inference of indentation style
 ;; (defun infer-indentation-style ()
 ;;   ;; if our source file uses tabs, we use tabs, if spaces spaces, and if        
@@ -16,12 +19,12 @@
 ;;     (if (> space-count tab-count) (setq indent-tabs-mode nil))
 ;;     (if (> tab-count space-count) (setq indent-tabs-mode t))))
 
-;; ;; don't accelerate scrolling
-;; (setq mouse-wheel-progressive-speed nil)
+;; don't accelerate scrolling
+(setq mouse-wheel-progressive-speed nil)
 
 
-;; ;; Delete selection mode
-;; (delete-selection-mode 1)
+;; Delete selection mode
+(delete-selection-mode 1)
 
 ;; ;; enable spell check by default
 ;; (add-hook 'text-mode-hook 'flyspell-mode)
@@ -30,16 +33,15 @@
 ;; auto refresh all buffers
 (global-auto-revert-mode 1)
 
-;; ;; line number in all buffers
-;; (global-linum-mode t)
+;; line number in all buffers
+(global-linum-mode t)
 ;; (add-hook 'doc-view-mode-hook (lambda() (linum-mode 0)))
 
-;; ;; show parenthesis matched
-;; (show-paren-mode t)
+;; show parenthesis matched
+(show-paren-mode t)
 
-;; change the highlight color
-(set-face-attribute 'region nil :background "#B9B9B9")
-
+;; ;; change the highlight color
+;; (set-face-attribute 'region nil :background "#99C4F7")
 
 ;; ;; right to left config
 ;; (defun set-bidi-env ()
