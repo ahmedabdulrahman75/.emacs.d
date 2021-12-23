2206b38cf1ec667d9e47baf9c2a496e51c7dd739
Ahmed Abdulrahman <ahmed@debian>
2021-12-23 14:14:11 +0200

    change theme

--- general-config.el
+++ general-config.el
@@ -40,8 +40,8 @@
 ;; show parenthesis matched
 (show-paren-mode t)
 
-;; ;; change the highlight color
-;; (set-face-attribute 'region nil :background "#99C4F7")
+;; change the highlight color
+(set-face-attribute 'region nil :background "#99C4F7")
 
 ;; ;; right to left config
 ;; (defun set-bidi-env ()
