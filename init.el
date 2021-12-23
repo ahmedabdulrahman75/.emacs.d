c4c4472bd91246f7ce0dfc49bebaf250173f79ca
Ahmed Abdulrahman <ahmed751995@riseup.net>
2021-12-23 14:24:25 +0200

    edit null to nil

--- init.el
+++ init.el
@@ -22,8 +22,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (null)))
- '(package-selected-packages (quote (markdown-mode markdown-mode+))))
+ '(custom-enabled-themes nil)
+ '(package-selected-packages '(markdown-mode markdown-mode+)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
