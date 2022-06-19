ec1a55ef8c4e4c1f5a36776a6272f90d0472061b
Ahmed Abdulrahman <ahmed751995@riseup.net>
2022-06-19 20:01:27 +0200

    auto pep8 when save python file

--- init.el
+++ init.el
@@ -5,6 +5,8 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
 ;; solve network problem
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 ;;--------------------------------------------------------------------------
@@ -13,7 +15,7 @@
 (load "~/.emacs.d/general-config") ; general configuration lisp file
 (add-to-list 'load-path "~/.emacs.d/lisp") ; packages configuration file
 (load "global")
-;; (load "python-lisp")
+(load "python-lisp")
 ;; (load "javascript-lisp")
 ;; (load "web-lisp")
 ;; (load "latex-lisp")
@@ -23,9 +25,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(adwaita))
+ '(custom-enabled-themes nil)
  '(package-selected-packages
-   '(indent-guide markdown-mode company yasnippet org yasnippet-snippets treemacs markdown-preview-mode impatient-mode company-quickhelp)))
+   '(py-autopep8 indent-guide markdown-mode company yasnippet org yasnippet-snippets treemacs markdown-preview-mode impatient-mode company-quickhelp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/python-lisp.el
+++ lisp/python-lisp.el
@@ -1,2 +1,10 @@
-(add-hook 'python-mode-hook '(lambda () 
- (setq python-indent 4)))
+;; requirements
+(package-install 'py-autopep8)
+
+
+;; py-autopep8
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+
+;; set default indentation value
+;; (add-hook 'python-mode-hook '(lambda () 
+;;  (setq python-indent 4)))
