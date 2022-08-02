9477ffbaa0ab6bd3fff9fd548f252aa5defbe5c5
Ahmed Abdulrahman <ahmed751995@riseup.net>
2022-08-02 21:53:59 +0200

    improve web mode

--- lisp/web-lisp.el
+++ lisp/web-lisp.el
@@ -9,13 +9,15 @@
 (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 
 ;; some configurations to web mode
-(setq web-mode-enable-current-column-highlight t)
+;; (setq web-mode-enable-current-column-highlight t)
 (setq web-mode-enable-current-element-highlight t)
 
 (defun my-web-mode-hook ()
   (set (make-local-variable 'company-backends)
        '(company-css company-web-html company-yasnippet company-files))
   (setq web-mode-markup-indent-offset 2) ;; set indentation to 2 spaces
+  (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "Black")
+  (set-face-attribute 'web-mode-html-tag-face nil :foreground "Blue")
 )
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
@@ -25,11 +27,3 @@
 
 ;; enable prettier
 (add-hook 'web-mode-hook 'prettier-mode)
-
-
-(defun tag-color-web-mode-hook ()
-  "Hooks for Web mode."
-  (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "Black")
-  (set-face-attribute 'web-mode-html-tag-face nil :foreground "Blue")
-  )
-(add-hook 'web-mode-hook  'tag-color-web-mode-hook)
