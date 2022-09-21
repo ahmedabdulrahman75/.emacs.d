9e59da612ad4855123c00cf3e9c16038da259eeb
Ahmed Abdulrahman <ahmed751995@riseup.net>
2022-09-21 17:25:27 +0200

    changed theme, added themes folder and changed web-mode tags highlight color

--- lisp/web-lisp.el
+++ lisp/web-lisp.el
@@ -15,8 +15,9 @@
   (set (make-local-variable 'company-backends)
        '(company-css company-web-html company-yasnippet company-files))
   (setq web-mode-markup-indent-offset 2) ;; set indentation to 2 spaces
-  (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "#2E3436")
-  (set-face-attribute 'web-mode-html-tag-face nil :foreground "#1A5FB4")
+  (set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "#D0CFCC")
+  (set-face-attribute 'web-mode-html-tag-face nil :foreground "#2A7BDE")
+  (set-face-background 'web-mode-current-element-highlight-face "#E9AD0C")
 )
 
 (add-hook 'web-mode-hook  'my-web-mode-hook)
