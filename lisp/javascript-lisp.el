8a565f09a46670efa62aaf6f4f787b29306ddb40
Ahmed Abdulrahman <ahmed751995@riseup.net>
2022-11-19 13:51:13 +0200

    improve docs

--- lisp/javascript-lisp.el
+++ lisp/javascript-lisp.el
@@ -1,19 +1,19 @@
 ;; requirements
 (package-install 'js2-mode)
-(package-install 'rjsx-mode)
 (package-install 'react-snippets)
 (package-install 'json-mode)
 (package-install 'typescript-mode)
 (package-install 'flymake-jslint)
+(package-install 'rjsx-mode)
 
 ;; js2-mode
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-;;rjsx mode
-(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
-
 ;; json-mode
 (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+
+;; rjsx-mode
+(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
 
 (add-hook 'typescript-mode-hook '(lambda ()
  (setq typescript-indent-level 2)))
