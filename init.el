7c46afb7696bd51636d78e39dcaaccd6112c6f20
Ahmed Abdulrahman <ahmed751995@riseup.net>
2023-02-10 19:11:33 +0200

    remove custom variables

--- init.el
+++ init.el
@@ -22,16 +22,5 @@
 ;; (load "sessions-lisp")
 ;; (load "latex-lisp")
 ;;--------------------------------------------------------------------------
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(add-node-modules-path js2-mode vscode-dark-plus-theme ivy-rich vterm swiper ivy all-the-icons markdown-mode company yasnippet yasnippet-snippets which-key web-mode typescript-mode treemacs switch-window smex rjsx-mode react-snippets pyvenv py-autopep8 multiple-cursors multi-vterm mode-icons markdown-preview-mode magit json-mode indent-guide impatient-mode format-all flymake-eslint exec-path-from-shell emmet-mode eglot editorconfig drag-stuff dotenv-mode doom-modeline counsel company-web company-quickhelp centaur-tabs auctex all-the-icons-ivy-rich)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; set custom variables in custom.el but never loaded
+(setq custom-file (concat user-emacs-directory "/custom.el"))
