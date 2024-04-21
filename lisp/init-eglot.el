3c34773197ab5c08bcf6797973f00d8568138eba
Ahmed Abdulrahman <ahmed751995@gmail.com>
2024-04-21 11:40:44 +0200

    improve python mode and docs

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,16 +1,8 @@
-(setq read-process-output-max (* 1024 1024))
-
 ;; eglot
-
-
 (use-package eglot
   :ensure t
   :config
   (setq eglot-confirm-server-initiated-edits nil)
-
-  (add-to-list 'eglot-server-programs
-               '(python-mode . ("jedi-language-server")))
-
   (add-hook 'prog-mode-hook 'eglot-ensure)
   :hook (eglot--managed-mode . (lambda () (company-mode t))))
 
--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,18 +1,2 @@
-;; py-autopep8
-;; (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
-
-(require 'python-mode)
-
-;; pyvenv
-(use-package pyvenv
-  :ensure t
-  :config
-  (pyvenv-activate "~/workspace/python/venv/"))
-
-
-;; set default indentation value
-(add-hook 'python-mode-hook '(lambda () 
- (setq python-indent 4)))
-
 (provide 'init-python)
 ;;; init-python.el ends here
