947a08537089a2fad7cc965a9d03faa2ed75b2dd
Ahmed Abdulrahman <ahmed751995@gmail.com>
2025-01-18 13:54:22 +0200

    set python and improve docs

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,5 +1,9 @@
 (add-hook 'python-mode-hook
-	  (lambda ()
-            (setq-default tab-width 4)))
+          (lambda ()
+            (setq python-indent 4)
+            (setq-default tab-width 4)
+            (setq python-indent-offset 4)
+            (setq indent-tabs-mode nil)))  ;; Use spaces instead of tabs
+
 (provide 'init-python)
 ;;; init-python.el ends here
